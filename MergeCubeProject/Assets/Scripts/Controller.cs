bb64ea867299ad20f33c911b3e8952055e367714
CalebAYamaura <caleb.yamamura@createthenext>
2019-08-06 16:30:41 -0400

    Tuesday2

---
+++ MergeCubeProject/Assets/Scripts/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // List of Variables
+    public float speed = 1f; // Speed of PacMan, can adjust
+    public gameObject spawn_1; // Escape Routes for off screen "hallways"
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)) // moving? Input?
+        {
+            transform.position += transform.forward * speed * Time.deltaTime; // Movement
+        }
+    }
+    void OnTriggerEnter(Collider gate)
+    {
+        if (gate.gameObject.tag == "Gate_1")
+        {
+            transform.position = spawn_1.transform.position;
+        }
+    }   
+}
